83fba158e896229b1f72572b43c83221ca8a9a75
blokern <52699234+blokern@users.noreply.github.com>
2020-06-24 17:20:08 +0200

    Add files via upload

---
+++ build/precache-manifest.c30bb1d9713bc5c0d5bcb6c2eb35ebeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33506ffb3b9c005b67ea4ac8d279dc6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44163671e10b94d9d298",
+    "url": "/static/css/main.4b4c288d.chunk.css"
+  },
+  {
+    "revision": "6aa6fb06a3fb660b6a4f",
+    "url": "/static/js/2.a782de3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a782de3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44163671e10b94d9d298",
+    "url": "/static/js/main.b5d5b9b3.chunk.js"
+  },
+  {
+    "revision": "89ac118f8d664424e2aa",
+    "url": "/static/js/runtime-main.40fd9fda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
